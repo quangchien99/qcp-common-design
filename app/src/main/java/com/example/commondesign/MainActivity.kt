9a681706f77c0ef6be2c4d4d1c17a47de7a59adc
Chien Pham <phamquangchien170499@gmail.com>
2023-01-17 15:25:17 +0700

    apply Elegant Dialog to sample app

--- app/src/main/java/com/example/commondesign/MainActivity.kt
+++ app/src/main/java/com/example/commondesign/MainActivity.kt
@@ -2,10 +2,143 @@ package com.example.commondesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.content.ContextCompat
+import com.qcp.common_design.dialog.elegant.ElegantDialog
+import com.qcp.common_design.dialog.elegant.background
+import com.qcp.common_design.dialog.elegant.body
+import com.qcp.common_design.dialog.elegant.icon
+import com.qcp.common_design.dialog.elegant.onNegative
+import com.qcp.common_design.dialog.elegant.onPositive
+import com.qcp.common_design.dialog.elegant.position
+import com.qcp.common_design.dialog.elegant.title
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val title = "Congratulations"
+        val body = "Your New Account has been created"
+        val goToMyAccount = "Go To My Account"
+        val cancel = "Cancel"
+        first.setOnClickListener {
+            ElegantDialog.build(this)
+                .title(title)
+                .body(body)
+        }
+        second.setOnClickListener {
+            ElegantDialog.build(this)
+                .title(title)
+                .body(body)
+                .icon(com.qcp.common_design.R.drawable.ic_congrts)
+        }
+        third.setOnClickListener {
+            ElegantDialog.build(this)
+                .title(title)
+                .body(body)
+                .icon(com.qcp.common_design.R.drawable.ic_congrts)
+                .onPositive(goToMyAccount) {
+                    Log.d("TAG", "positive ")
+                }
+        }
+        fourth.setOnClickListener {
+            ElegantDialog.build(this)
+                .title(title)
+                .body(body)
+                .icon(com.qcp.common_design.R.drawable.ic_congrts)
+                .onPositive(goToMyAccount) {
+                    Log.d("TAG", "positive ")
+                }
+                .onNegative(cancel) {
+                    Log.d("TAG", "negative ")
+                }
+        }
+        fifth.setOnClickListener {
+            ElegantDialog.build(this)
+                .title(title)
+                .body(body)
+                .onPositive(goToMyAccount) {
+                    Log.d("TAG", "positive ")
+                }
+        }
+        sixth.setOnClickListener {
+            ElegantDialog.build(this)
+                .title(title)
+                .body(body)
+                .onPositive(goToMyAccount) {
+                    Log.d("TAG", "positive ")
+                }
+                .onNegative(cancel) {
+                    Log.d("TAG", "negative ")
+                }
+        }
+        seventh.setOnClickListener {
+            ElegantDialog.build(this)
+                .title(
+                    title,
+                    titleColor = ContextCompat.getColor(this, android.R.color.white)
+                )
+                .body(
+                    body,
+                    color = ContextCompat.getColor(this, android.R.color.white)
+                )
+                .background(R.drawable.layout_rounded_green)
+                .onPositive(
+                    goToMyAccount,
+                    buttonBackgroundColor = R.drawable.layout_rounded_dark_white,
+                    textColor = ContextCompat.getColor(this, android.R.color.black)
+                ) {
+                    Log.d("TAG", "positive ")
+                }
+                .onNegative(
+                    cancel,
+                    buttonBackgroundColor = R.drawable.layout_rounded_dark_white,
+                    textColor = ContextCompat.getColor(this, android.R.color.black)
+                ) {
+                    Log.d("TAG", "negative ")
+                }
+        }
+        eighth.setOnClickListener {
+            ElegantDialog.build(this)
+                .title(
+                    title,
+                    titleColor = ContextCompat.getColor(this, android.R.color.white)
+                )
+                .body(
+                    body,
+                    color = ContextCompat.getColor(this, android.R.color.white)
+                )
+                .icon(com.qcp.common_design.R.drawable.ic_congrts)
+                .background(R.drawable.layout_rounded_green)
+                .onPositive(
+                    goToMyAccount,
+                    buttonBackgroundColor = R.drawable.layout_rounded_dark_white,
+                    textColor = ContextCompat.getColor(this, android.R.color.black)
+                ) {
+                    Log.d("TAG", "positive ")
+                }
+        }
+        nine.setOnClickListener {
+            ElegantDialog.build(this)
+                .title(
+                    title,
+                    titleColor = ContextCompat.getColor(this, android.R.color.white)
+                )
+                .body(
+                    body,
+                    color = ContextCompat.getColor(this, android.R.color.white)
+                )
+                .icon(com.qcp.common_design.R.drawable.ic_congrts)
+                .background(R.drawable.layout_rounded_green)
+                .position(ElegantDialog.POSITIONS.CENTER)
+                .onPositive(
+                    goToMyAccount,
+                    buttonBackgroundColor = R.drawable.layout_rounded_dark_white,
+                    textColor = ContextCompat.getColor(this, android.R.color.black)
+                ) {
+                    Log.d("TAG", "positive ")
+                }
+        }
     }
 }
